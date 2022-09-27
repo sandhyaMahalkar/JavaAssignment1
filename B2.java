b1829c90a65777cdd267b348ad6fde732eef6b49
sandhyaMahalkar <114503392+sandhyaMahalkar@users.noreply.github.com>
2022-09-27 20:58:53 +0530

    Create B2.java

---
+++ B2.java
@@ -0,0 +1,18 @@
+//Question: 2)Write a program to display addition of two numbers on screen.
+
+//Code:-
+class addition
+{
+  public static void main(String[] args) {
+    int a,b,sum;
+    a=10;
+    b=20;
+    sum=a+b;
+    System.out.println("Addition = "+sum);
+  }
+}
+
+/*Output:-
+Addition = 30
+
+Process finished with exit code 0*/
